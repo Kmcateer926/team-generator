50abdf39542e48b33d2d69406fd785906f631599
Kevin McAteer <kmcateer926@gmail.com>
2020-11-11 21:38:18 -0500

    added role selection

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const teamArray = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -33,3 +33,12 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+const roleSelect = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"],
+    }
+];
+
